4ab4eb06c1d346d6fbc2d945b67589432eb4d6b5
Alexander <pchp250g8@tutamail.com>
2026-01-30 15:48:40 +0300

    Добавьте файлы проекта.

---
+++ SymbolExchange/Program.cs
@@ -0,0 +1,9 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Input a string");
+var strLine1 = Console.ReadLine();
+var nLen = strLine1.Length;
+var strLine2 = strLine1[nLen - 1] + strLine1.Substring(1,nLen - 2) + strLine1[0];
+Console.WriteLine("Exchange the first and the last symbol in the string");
+Console.WriteLine("Source string:{0}", strLine1);
+Console.WriteLine("Destinstion string:{0}", strLine2);
+Console.Read();
